eaebb9b1e7809d78994bc8b0823195ee3f8a3b1e
Leandropereira30 <leandropereira1932@outlook.com>
2025-06-28 14:14:15 +0000

    printf("Movimento da Torre:\n"); printf("Direita\n"); printf("\nMovimento do Bispo:\n"); printf("Cima, Direita\n"); printf("\nMovimento da Rainha:\n"); printf("Esquerda\n");

--- xadrez.c
+++ xadrez.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
-int main(){
-    printf("desafio xadrez!\n");
-}
-
+int main() {
     // ========================
     // Movimento da TORRE (for)
     // ========================
